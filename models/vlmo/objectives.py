fix softmax temp nan loss

models/vlmo/objectives.py
@@ -47,13 +47,16 @@ def compute_mlm(model, batch):
     mlm_logits = model.mlm_head(infer["txt_feats"])
     mlm_labels = infer["txt_labels"]
 
-    mlm_loss = F.cross_entropy(
-        mlm_logits.view(-1, model.config.model.vocab_size),
-        mlm_labels.view(-1),
-        ignore_index=-100,
-    )
-
     mlm_mean_acc, mlm_count = compute_accuracy(mlm_logits, mlm_labels)
+
+    if mlm_count > 0:
+        mlm_loss = F.cross_entropy(
+            mlm_logits.view(-1, model.config.model.vocab_size),
+            mlm_labels.view(-1),
+            ignore_index=-100,
+        )
+    else:
+        mlm_loss = 0.
 
     ret = {
         "mlm_task_loss": mlm_loss,
@@ -68,7 +71,7 @@ def compute_mlm(model, batch):
 
 def compute_itc(model, batch):
     with torch.no_grad():
-        model.itc_temp.clamp_(0.01, 0.5)
+        model.itc_temp.data = torch.clamp(model.itc_temp.data, 0, 4.6052)
 
     img_infer = model.infer(batch, infer_mode='img_only')
     txt_infer = model.infer(batch, infer_mode='txt_only')
@@ -78,6 +81,9 @@ def compute_itc(model, batch):
 
     i_feat = model.itc_head(i_feat)
     t_feat = model.itc_head(t_feat)
+
+    i_feat = i_feat / i_feat.norm(dim=-1, keepdim=True)
+    t_feat = t_feat / t_feat.norm(dim=-1, keepdim=True)
     '''
     if model.img_queue is not None and model.txt_queue is not None:
         with torch.no_grad():
@@ -105,7 +111,12 @@ def compute_itc(model, batch):
                 sim_targets = torch.eye(*sim_i2t_m.size(), device=i_feat.device)
     '''
 
-    sim_i2t = i_feat @ t_feat.t() / model.itc_temp
+    raw_sim_i2t = i_feat @ t_feat.t()
+    raw_sim_t2i = raw_sim_i2t.t()
+
+    temp = model.itc_temp.exp()
+
+    sim_i2t = raw_sim_i2t * temp
     sim_t2i = sim_i2t.t()
 
     sim_targets = torch.arange(sim_i2t.size(0), device=sim_i2t.device)
@@ -120,7 +131,7 @@ def compute_itc(model, batch):
         't2i_Loss': t2i_loss,
         'sim_i2t': sim_i2t,
         'sim_t2i': sim_t2i,
-        'itc_temp': model.itc_temp,
+        'itc_temp': temp.data,
     }
     return ret
 

models/vlmo/vlmo_module.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from functools import partial
 
+import numpy as np
 import torch
 import torch.nn as nn
 from timm.utils import ModelEmaV2 as ModelEma
@@ -55,8 +56,8 @@ class VlmoModule(nn.Module):
         if 'itc' in self.loss_names:
             self.itc_head = ITCHead(hs)
             self.itc_head.apply(self.transformer._init_weights)
-            self.itc_temp = nn.parameter.Parameter(
-                torch.tensor(config.model.temp))
+            self.itc_temp = nn.Parameter(
+                torch.ones([]) * np.log(1 / config.model.temp))
 
         if 'itm' in self.loss_names:
             self.itm_head = ITMHead(hs)

train/pretrain/multimodal.py
@@ -59,13 +59,25 @@ def pretrain_mum(cfg: DictConfig, logger: Logger):
 
     model.cuda()
     if cfg.deepspeed.enable:
+        if cfg.deepspeed.pth2ds is not None:
+            torch_ckpt = torch.load(cfg.deepspeed.pth2ds, map_location='cpu')
+            model.load_from_ckpt(torch_ckpt)
+            cfg.train.auto_resume = False
+
         model, optimizer, _, _ = ds_init(
             model=model,
             model_parameters=[p for p in model.parameters() if p.requires_grad],
             dist_init_required=not cfg.dist.distributed,
             config=OmegaConf.to_object(cfg.deepspeed.config),
         )
+        model_without_ddp = model.module
         loss_scaler = None
+
+        if cfg.deepspeed.pth2ds is not None:
+            logger.warning(
+                f'Init Deepspeed with torch state_dict: {cfg.deepspeed.pth2ds},'
+                f' and disable auto resume ckpt as default.')
+
     else:
         if cfg.dist.distributed:
             model = torch.nn.parallel.DistributedDataParallel(
@@ -259,8 +271,22 @@ def train_one_epoch(model: torch.nn.Module,
             with torch.cuda.amp.autocast():
                 outputs = model(batch)
 
-        total_loss = sum([v for k, v in outputs.items() if "task_loss" in k])
+        # FIXME Image Text Matching may cause Nan Loss in any iteration
+
+        total_loss = sum([
+            v for k, v in outputs.items()
+            if "task_loss" in k and math.isfinite(v)
+        ])
         # __import__('ipdb').set_trace()
+
+        for k, v in outputs.items():
+            if 'loss' in k and not math.isfinite(v):
+                logger.warning(f"{k} is {v}, but not stop training")
+                logger.warning(f"\n{outputs}")
+                torch.save(
+                    outputs,
+                    os.path.join(cfg.output_dir,
+                                 f"{cfg.dist.rank}_{it}_nan_obj.pth"))
 
         if not math.isfinite(total_loss):
             logger.warning(f"Loss is {total_loss}, stopping training")
@@ -306,6 +332,7 @@ def train_one_epoch(model: torch.nn.Module,
         if 'itm_mean_acc' in outputs.keys():
             metrics.update(itm_acc=dict(value=outputs['itm_mean_acc'].item(),
                                         n=outputs['itm_count']),)
+            metrics.update(itc_temp=outputs['itc_temp'])
 
         opts = dict()
 

train/pretrain/text.py
@@ -258,8 +258,20 @@ def train_one_epoch(model: torch.nn.Module,
             with torch.cuda.amp.autocast():
                 outputs = model(batch)
 
-        total_loss = sum([v for k, v in outputs.items() if "task_loss" in k])
+        total_loss = sum([
+            v for k, v in outputs.items()
+            if "task_loss" in k and math.isfinite(v)
+        ])
         # __import__('ipdb').set_trace()
+
+        for k, v in outputs.items():
+            if 'loss' in k and not math.isfinite(v):
+                logger.warning(f"{k} is {v}, but not stop training")
+                logger.warning(f"\n{outputs}")
+                torch.save(
+                    outputs,
+                    os.path.join(cfg.output_dir,
+                                 f"{cfg.dist.rank}_{it}_nan_obj.pth"))
 
         if not math.isfinite(total_loss):
             logger.warning(f"Loss is {total_loss}, stopping training")